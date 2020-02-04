a2f42d447e900c9492ee76963a21887a4e19c1dd
zhaozhao <zhaozhao@mail.taiji.com.cn>
2020-02-04 17:27:47 +0800

    Add:init

--- README.md
+++ README.md
@@ -1,10 +1,15 @@
-# oauth2-client-login-js-github
-公众号文章《》配套代码，js实现Github登录
+### oauth2-client-login-js-github
+公众号文章《》配套代码
 
-# 在github注册的OAuth App
-https://github.com/settings/applications/1214669
-
-### 运行
+### 运行与演示
 1. 安装nodejs环境
 1. 安装依赖的request工具包：`npm install --save request`
-1. 执行:`node server.js`
+1. 运行:`node server.js`
+1. 打开浏览器访问：http://localhost:8080/loginGithub.html
+1. 点击按钮：`使用Github登录`
+
+### 注意
+1. 为方便大家运行此示例程序，代码中暴露了client_secret，关联的OAuth App地址：https://github.com/settings/applications/1214669。
+望不要将此密钥用于非法用途，发现必究。
+1. 大家最好在Github上注册各自的OAuth App。
+   
--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-const http = require('http'); //用来启服务
+const http = require('http'); //用来启动web服务
 const fs = require('fs'); //用来读取文件
 const url = require("url");//用来获取url中的参数
 const request = require('request');//用来发起网络请求
@@ -10,29 +10,45 @@ const server = http.createServer(function (req, res) {
     });
 
     if (req.url === "/loginGithub.html") {
+        //返回页面
         fs.readFile("./loginGIthub.html", function (err, data) {
             res.write(data);
-            res.end();//将index.html显示在客户端
+            res.end();
         })
     } else {
+        //获取回调地址中的code
         const arg = url.parse(req.url, true).query;
         //回调地址接收code
         if (arg && arg.code) {
             //如果参数中有code，说明是GitHub通过重定向传递code,携带code请求token
-            const tokenUrl = "https://github.com/login/oauth/access_token?client_id=1994fe209a6970a75a26" +
-            "&client_secret=1c0de0b18501cfa419a250ebe855207ba6d2b6d7" +
-            "&redirect_uri=http://localhost:8080" +
-            "&state=" + arg.state +
-            "&code=" + arg.code;
+            const tokenUrl = "https://github.com/login/oauth/access_token" +
+                "?client_id=1994fe209a6970a75a26" +
+                "&client_secret=1c0de0b18501cfa419a250ebe855207ba6d2b6d7" +
+                "&redirect_uri=http://localhost:8080" +
+                "&state=" + arg.state +
+                "&code=" + arg.code;
 
-            //后台请求github的token
+            //1 后台请求github的token
             request.post({url: tokenUrl}, function optionalCallback(err, httpResponse, body) {
                 if (err) {
-                    res.write('<h1>获取token失败</h1><p>err</p>');
+                    res.write('<h1>获取token失败</h1><p>' + err + '</p>');
                     res.end();
-                }else{
-                    res.write('<h1>获取token成功</h1><p>body</p>');
-                    res.end();
+                } else {
+                    //获取token成功
+                    let token = url.parse('?' + body, true).query.access_token;
+                    //2 使用token访问用户在github的信息
+                    request({url: 'https://api.github.com/user', headers: {
+                            'Authorization': 'token ' + token, //携带token
+                            'User-Agent': 'example' //github要求必须设置User-Agent
+                        }}, function (error, response, b) {
+                        if (error) {
+                            res.write('<h1>获取token成功,但是使用token获取用户信息失败</h1><p>' + body + '</p>');
+                            res.end();
+                        } else {
+                            res.write('<h1>获取token成功,使用token获取用户信息成功</h1><p>' + body + '</p><p>' + b + '</p>');
+                            res.end();
+                        }
+                    })
                 }
             });
         } else {
