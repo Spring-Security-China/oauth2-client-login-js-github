bbde9204ed5ba9b08dc44cdafee31d1635c1e9c8
zhaozhao <zhaozhao@mail.taiji.com.cn>
2021-03-12 11:03:43 +0800

    Mod:因为国内有时候访问Github速度较慢，所以同时准备了码云的代码示例。

--- README.md
+++ README.md
@@ -1,7 +1,19 @@
 ### oauth2-client-login-js-github
 公众号文章[《最简洁实现Github登录的JS代码示例》](https://mp.weixin.qq.com/s/aHwcOwNpGjYZc1VBzSENYw)配套代码
 
+### 代码结构说明：
+因为国内有时候访问Github速度较慢，所以同时准备了码云的代码示例。
+1. 使用码云Gitee作为OAuth2 Server
+    * loginGitee.html：前端
+    * server-gitee.js: 后端
+1. 使用Github作为OAuth2 Server
+    * loginGithub.html：前端
+    * server.js: 后端
+    
+两组代码示例分别运行即可。以Github代码组为主，如果访问Github较慢可以使用码云Gitee。
+
 ### 运行与演示
+（以Github组示例为主）
 1. 安装nodejs环境
 1. 安装依赖的request工具包：`npm install --save request`
 1. 运行:`node server.js`
@@ -12,3 +24,4 @@
 1. 为方便大家运行此示例程序，代码中暴露了client_secret，关联的OAuth App地址：https://github.com/settings/applications/1214669。
 望不要将此密钥用于非法用途，发现必究。
 1. 大家最好在Github上注册各自的OAuth App。
+   
--- server-gitee.js
+++ server-gitee.js
@@ -9,9 +9,9 @@ const server = http.createServer(function (req, res) {
         'content-type': 'text/html;charset="utf-8"'
     });
 
-    if (req.url === "/loginGithub.html") {
+    if (req.url === "/loginGitee.html") {
         //返回页面
-        fs.readFile("./loginGithub.html", function (err, data) {
+        fs.readFile("./loginGitee.html", function (err, data) {
             res.write(data);
             res.end();
         })
@@ -20,12 +20,13 @@ const server = http.createServer(function (req, res) {
         const arg = url.parse(req.url, true).query;
         //回调地址接收code
         if (arg && arg.code) {
-            debugger
+
             //如果参数中有code，说明是GitHub通过重定向传递code,携带code请求token
-            const tokenUrl = "https://github.com/login/oauth/access_token" +
-                "?client_id=1994fe209a6970a75a26" +
-                "&client_secret=1c0de0b18501cfa419a250ebe855207ba6d2b6d7" +
-                "&redirect_uri=http://localhost:8080" +
+            const tokenUrl = "https://gitee.com/oauth/token" +
+                "?client_id=0391e38b3f2017d77eb835d9a576b32828c554a38c0b45ecfa5ac2a57c96b470" +
+                "&client_secret=35986c47881c7daf5c552030a4e06e1520030889b825569f8bb22c14151ea2df" +
+                "&redirect_uri=http://localhost:8084" +
+                "&grant_type=authorization_code" +
                 "&state=" + arg.state +
                 "&code=" + arg.code;
 
@@ -35,13 +36,14 @@ const server = http.createServer(function (req, res) {
                     res.write('<h1>获取token失败</h1><p>' + err + '</p>');
                     res.end();
                 } else {
+                    // res.write('<h1>获取token成功</h1><p>' + body + '</p>');
+                    // res.end();
                     //获取token成功
-                    let token = url.parse('?' + body, true).query.access_token;
+                    let accessToken = JSON.parse(body).access_token
+                    
                     //2 使用token访问用户在github的信息
-                    request({url: 'https://api.github.com/user', headers: {
-                            'Authorization': 'token ' + token, //携带token
-                            'User-Agent': 'example' //github要求必须设置User-Agent
-                        }}, function (error, response, b) {
+                    request({url: 'https://gitee.com/api/v5/user?access_token='+accessToken}
+                    , function (error, response, b) {
                         if (error) {
                             res.write('<h1>获取token成功,但是使用token获取用户信息失败</h1><p>' + body + '</p>');
                             res.end();
@@ -53,10 +55,10 @@ const server = http.createServer(function (req, res) {
                 }
             });
         } else {
-            res.write('<h1>404</h1><p>此示例只有 http://localhost:8080/loginGithub.html 一个页面</p>');
+            res.write('<h1>404</h1><p>此示例只有 http://localhost:8084/loginGitee.html 一个页面</p>');
             res.end();
         }
     }
-}).listen(8080); //端口号
+}).listen(8084); //端口号
 
-console.log('服务器开启成功,请访问：http://localhost:8080/loginGithub.html');
+console.log('服务器开启成功,请访问：http://localhost:8084/loginGitee.html');
